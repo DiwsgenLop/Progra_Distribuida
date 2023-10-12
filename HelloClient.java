cf4ddb51ae8292d467029d137da08cd51eaa50f7
DiwsgenLop <diwsgen.lopez@alumno.buap.mx>
2023-10-12 13:38:28 -0600

    The user can put in any string

--- HelloClient.java
+++ HelloClient.java
@@ -2,18 +2,18 @@ import java.rmi.*;
 
 public class HelloClient {
   public static void main(String[] args) {
-    if (args.length < 1) {
-      System.out.println("Uso: HelloClient <ip>");
+    if (args.length < 2) {
+      System.out.println("Uso: HelloClient <ip> <frase>");
       return;
     }
 
+    String serverIP = args[0];
+    String phrase = args[1];
+
     try {
-      String serverIP = args[0];
       HelloInterface hello = (HelloInterface) Naming.lookup("//" + serverIP + "/Hello");
       System.out.println(hello.say());
 
-      // Llamada al método countVowels
-      String phrase = "Hola, qué tal?";
       int numVowels = hello.countVowels(phrase);
       System.out.println("Número de vocales en la frase: " + numVowels);
     } catch (Exception e) {
